a651f2f04a9961c4cf429d377f2741e15e234809
unknown <bodawahid456@gmail.com>
2024-12-22 00:05:27 +0200

    editing

--- spaceinvaders.d
+++ spaceinvaders.d
@@ -3,3 +3,6 @@
 .\spaceinvaders.o: Nokia5110.h
 .\spaceinvaders.o: Random.h
 .\spaceinvaders.o: TExaS.h
+.\spaceinvaders.o: C:\Keil\ARM\ARMCC\bin\..\include\time.h
+.\spaceinvaders.o: C:\Keil\ARM\ARMCC\bin\..\include\stdlib.h
+.\spaceinvaders.o: C:\Keil\ARM\ARMCC\bin\..\include\stdio.h
